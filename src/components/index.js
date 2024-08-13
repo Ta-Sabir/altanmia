23b7c6c3bf6d7f6733cc7b6d230528a07ec02585
Saber Ai <115227518+Ta-Sabir@users.noreply.github.com>
2024-08-13 21:36:24 +0200

    Delete src directory

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-import Hero from "./Hero/Hero";
-import Navbar from "./Navbar/Navbar";
-import Courses from './Courses/Courses';
-export { Hero, Navbar,Courses };
--- src/pages/index.js
+++
@@ -1,6 +0,0 @@
-import Home from './Home/Home';
-import Languages from './Languages/Languages';
-import Psychology from './Psychology/Psychology';
-import Other from './Other/Other';
-
-export { Home, Languages, Psychology,Other }
